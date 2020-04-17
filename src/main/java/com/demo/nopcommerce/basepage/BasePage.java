4797420bbd065db0a94a55b2e0c09c9b94ccf2e0
John Carry <carryjohn071@gmail.com>
2020-04-17 16:16:21 +0100

    sample-page-factory-project

---
+++ src/main/java/com/demo/nopcommerce/basepage/BasePage.java
@@ -0,0 +1,14 @@
+package com.demo.nopcommerce.basepage;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by Jay
+ */
+public class BasePage {
+
+    public static WebDriver driver;
+
+
+
+}
